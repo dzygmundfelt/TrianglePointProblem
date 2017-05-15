9c2b5ac2824b49fd8fc88104526bc491bf28e18b
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-15 17:58:52 -0400

    Dividing TriangleSplitter tasks up into helper methods.

--- src/main/java/zygmundfelt/dan/trianglepointproblem/TriangleSplitter.java
+++ src/main/java/zygmundfelt/dan/trianglepointproblem/TriangleSplitter.java
@@ -1,38 +1,66 @@
 package zygmundfelt.dan.trianglepointproblem;
 
+import java.util.ArrayList;
+
 public class TriangleSplitter {
 
     private Triangle triangle;
     private boolean hasHorizontalLine;
-    private Line dividingLine;
+    private HorizontalLineSegment dividingLine;
 
     TriangleSplitter(Triangle triangle) {
         this.triangle = triangle;
-        setHasHorizontalLine();
-    }
-
-    public void setHasHorizontalLine() {
-        hasHorizontalLine =  triangle.line1.getSlope()
-                * triangle.line2.getSlope()
-                * triangle.line3.getSlope() == 0;
+        setDividingLine();
     }
 
     public void setDividingLine() {
-        if(hasHorizontalLine) {
-            if(triangle.p1.getY() == triangle.p2.getY()) {
-                dividingLine = triangle.line1;
-            } else if(triangle.p2.getY() == triangle.p3.getY()) {
-                dividingLine = triangle.line2;
-            } else {
-                dividingLine = triangle.line3;
+        if(triangle.line1.getSlope() == 0) {
+            hasHorizontalLine = true;
+            dividingLine = new HorizontalLineSegment(triangle.p1.getY(), triangle.p1.getX(), triangle.p2.getX());
+        } else if(triangle.line2.getSlope() == 0) {
+            hasHorizontalLine = true;
+            dividingLine = new HorizontalLineSegment(triangle.p2.getY(), triangle.p2.getX(), triangle.p3.getX());
+        } else if(triangle.line3.getSlope() == 0) {
+            hasHorizontalLine = true;
+            dividingLine = new HorizontalLineSegment(triangle.p1.getY(), triangle.p1.getX(), triangle.p3.getX());
+        } else {
+            hasHorizontalLine = false;
+            createDividingLine();
+        }
+    }
+
+    public void createDividingLine() {
+
+        //get the line that doesn't contain middleYPoint
+
+        //find the horizontal line segment between mYP and that line
+
+    }
+
+
+    /*
+    This is an ugly helper method. Maybe there's a better way to do this using a Comparator. TODO
+     */
+    public Point findMiddleYPoint() {
+        ArrayList<Point> list = new ArrayList<Point>();
+        list.add(triangle.p1);
+        list.add(triangle.p2);
+        list.add(triangle.p3);
+        double maxY = Math.max(triangle.p1.getY(), Math.max(triangle.p2.getY(), triangle.p3.getY()));
+        for(int i = 0; i < 3; i++) {
+            if(list.get(i).getY() == maxY) {
+                list.remove(i);
+                break;
             }
         }
-
-        double maxY = Math.max(triangle.p1.getY(), Math.max(triangle.p2.getY(), triangle.p3.getY()));
-
-        if(maxY == triangle.p1.getY()) {
-            //dividingLine = new Line(triangle.p1, )
+        double minY = Math.min(triangle.p1.getY(), Math.min(triangle.p2.getY(), triangle.p3.getY()));
+        for(int i = 0; i < 2; i++) {
+            if(list.get(i).getY() == minY) {
+                list.remove(i);
+                break;
+            }
         }
+        return list.get(0);
     }
 
 }
